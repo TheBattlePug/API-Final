cde28ab9b9809cbda173afdffd1b612aa72786a8
Dan Dumitrescu <dan.dumitrescu@LIVE.COM>
2021-12-20 13:32:40 -0800

    Finally fixed my problem, I just changed the environment to miniconda3 instead.

---
+++ API-Final/API_Final.py
@@ -0,0 +1,29 @@
+import requests
+
+"""
+#here is the part where you can request an article and its elements
+r = requests.get('https://xkcd.com/1906/')
+
+print(r.status_code)
+
+print(r.headers)
+
+print('\n' + r.headers['Content-Type'])
+
+print('\n' + r.text)
+
+#image
+recieve = requests.get('https://imgs.xkcd.com/comics/mkaing_progress.png')
+
+with open(r'C:\Inputs\DansCat.jpg','wb') as f:
+    f.write(recieve.content)
+
+print("\n It works ... finally")
+"""
+
+#passing argument in the request
+
+ploads = {'things':2, 'total': 25}
+r = requests.get('https://httpbin.org/get', params=ploads)
+print(r.text)
+print(r.url)
