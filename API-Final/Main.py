13ef68e76c022e7fa486ccd80fdfd844e9197c5a
Dan Dumitrescu <57470203+TheBattlePug@users.noreply.github.com>
2021-12-25 17:34:43 -0800

    removed venv file from the folder, so it is gone and we do not have to worry about it. (the program still works)

--- API-Final/Main.py
+++ API-Final/Main.py
@@ -1,11 +1,17 @@
 import requests
+import re
 
 def print_whole_website(link):
-    r = requests.get(link)
+    r = requests.get(link)    
     print(r.text + '\n')
     return
 
+
 def print_headers(link):
     r = requests.get(link)
-    print(r.headers + '\n')
-    return+
+    print(r.headers['Content-Type'] + '\n')
+    return
+
+
+print_whole_website('http://www.sosmath.com/trig/Trig5/trig5/trig5.html')
+ 
