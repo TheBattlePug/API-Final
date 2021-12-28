911f4af44d39254ac0d8d6505d709a93c326b49c
Dan Dumitrescu <57470203+TheBattlePug@users.noreply.github.com>
2021-12-27 21:59:33 -0800

    Trying to create a method to get the image links

--- API-Final/Main.py
+++ API-Final/Main.py
@@ -1,4 +1,3 @@
-"""
 import requests
 import re
 
@@ -13,6 +12,26 @@ def print_headers(link):
     print(r.headers['Content-Type'] + '\n')
     return
 
+def get_image_links(link):
+    r = requests.get(link)
+    website_code = r.text
 
-print_whole_website('http://www.sosmath.com/trig/Trig5/trig5/trig5.html') 
-"""
+    image_links = []
+
+    last_index = website_code.index("<p>")    
+    while last_index < len(website_code):
+        index_of_image_begin = website_code.find("<img", last_index) 
+        index_of_image_end = website_code.find(">", index_of_image_begin) + 2
+
+        if(index_of_image_end < len(website_code)):
+            image_link = website_code[index_of_image_begin : website_code.find(">", index_of_image_begin)]
+            image_links.append(image_link)
+            print(image_link)
+
+        last_index = index_of_image_end
+    
+    return
+
+   
+
+get_image_links('http://www.sosmath.com/trig/Trig5/trig5/trig5.html')
